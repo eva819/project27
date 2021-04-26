f78a472282b896ae89d1f0d012f95a29faa6dc0d
eva819 <82939076+eva819@users.noreply.github.com>
2021-04-26 11:17:56 +0530

    Add files via upload

--- rope.js
+++ rope.js
@@ -21,7 +21,7 @@ class rope{
         var Anchor1Y=pointA.y
 
         var Anchor2X=pointB.x+this.offsetX
-        var Anchor2Y=pointB.y+yhis.offsetY
+        var Anchor2Y=pointB.y+this.offsetY
 
         line(Anchor1X, Anchor1Y , Anchor2X , Anchor2Y);
     }
